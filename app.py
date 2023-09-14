aa711d4ab450dceec64d8203fa6957174bae119d
Frédéric Dignard <frederic.dignard@gmail.com>
2023-09-14 08:21:21 -0400

    Ajout de mongoDB

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from dotenv import dotenv_values
+
+config = dotenv_values(".env")
+
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = config['CONNEXION_BD']
+mongo = PyMongo(app)
+
+produits = mongo.db.products.find()
+
 
 @app.route("/")
 def index():
