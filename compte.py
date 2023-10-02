8bbe8cbee399815290be980f742139447cb10fdc
Lepadblond <girard.djs@gmail.com>
2023-10-02 16:22:43 -0400

    debug db j<ai un probleme avec le find encore

--- compte.py
+++ compte.py
@@ -1,6 +1,6 @@
 import re
 from flask import redirect, render_template, request, Blueprint, session, abort
-import hashlib
+import hashlib , logging
 import app
 
 bp_compte = Blueprint("compte", __name__)
@@ -20,25 +20,21 @@ def connection():
     if request.method == "GET":
         # Affiche la page de formulaire de connexion lors de la requête GET
         return render_template("compte/login.jinja")
-    elif request.method == "POST":
+    else:
         # Traite les données du formulaire de connexion lors de la requête POST
         mail = request.form.get("mail", default="")
-        mdp = request.form.get("motdepasse", default="")
+        mdp = request.form.get("mdp", default="")
 
-        user = app.mongo.Users.find_one({"email": mail}, {"password": mdp})\
-
-
-        if not user:
-            abort(401)
-
-        creer_session(user["id"])
+        user = app.mongo.db.users.find_one({"email": mail}, {"password": mdp})
+        logging.info(user)
+        creer_session(user["email"])
 
         return render_template("/")
 
 
 # Fonction pour créer une session utilisateur
-def creer_session(identifiant):
-    user = app.mongo.Users.find_one({"_id": identifiant})
+def creer_session(email):
+    user = app.mongo.db.users.find_one({"email": email})
     if not user:
         return redirect("/authentifier", code=303)  # Redirige vers la page de connexion en cas d'échec
     else:
@@ -51,6 +47,8 @@ def creer_session(identifiant):
 @bp_compte.route("/deconnecter")
 def deconnection():
     # ... Ajoutez ici la logique de déconnexion et de suppression de la session
+    session.pop("user", None)
+
     return redirect("/", code=303)  # Redirige vers la page d'accueil après la déconnexion
 
 
@@ -61,7 +59,7 @@ def inscription():
     if request.method == 'GET':
         # Affiche la page de formulaire d'inscription lors de la requête GET
         return render_template('compte/inscription.jinja', message={})
-    elif request.method == 'POST':
+    else:
         # Traite les données du formulaire d'inscription lors de la requête POST
         nom = request.form.get('nom')
         prenom = request.form.get('prenom')
@@ -72,12 +70,12 @@ def inscription():
         message = {}
 
         # Validation des données du formulaire (à décommenter et à compléter)
-        if not nom or not mail or not mdp or not mdp2 or not prenom:
-            message['vide'] = 'Veuillez remplir tous les champs'
-        if mdp != mdp2:
-            message['mdp'] = 'Les mots de passe ne correspondent pas'
-        if not re.match(regex_courriel, mail):
-            message['mail'] = 'Adresse mail invalide'
+        # if not nom or not mail or not mdp or not mdp2 or not prenom:
+        #     message['vide'] = 'Veuillez remplir tous les champs'
+        # if mdp != mdp2:
+        #     message['mdp'] = 'Les mots de passe ne correspondent pas'
+        # if not re.match(regex_courriel, mail):
+        #     message['mail'] = 'Adresse mail invalide'
 
         if message != {}:
             # Si des erreurs sont présentes, affiche le formulaire avec les messages d'erreur
@@ -90,10 +88,10 @@ def inscription():
             app.mongo.db.users.insert_one({"nom": nom, "prenom": prenom, "email": mail, "password": mdp})
 
             # Récupération de l'ID de l'utilisateur nouvellement inscrit
-            id_user = app.mongo.Users.find_one({"email": mail})
+            #id_user = app.mongo.db.users.find_one({"email": mail})
 
             # Création d'une session pour l'utilisateur inscrit
-            creer_session(id_user["id"])
+           # creer_session(id_user["_id"])
 
             # Redirige vers la page d'accueil après l'inscription
             return redirect('/', code=303)
