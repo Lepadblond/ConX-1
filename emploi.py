1c2a612ef860e1d5fe1b051d2b3d5cff7c835e25
Lepadblond <girard.djs@gmail.com>
2023-11-21 15:50:50 -0500

    emploi

--- emploi.py
+++ emploi.py
@@ -36,9 +36,63 @@ def afficherlemploi(id_emploi):
         # je recupere l'emploi dans la base de données
         object_id = ObjectId(id_emploi)
         emploi = app.mongo.db.emplois.find_one({"_id": object_id})
-        print(emploi)
 
         # je retourne la page de l'emploi avec les données
         return render_template("emploi/emploidetail.jinja", emploi=emploi)
 
+
 # Route pour ajouter un emploi
+@bp_emploi.route("/ajouter", methods=["GET", "POST"])
+def ajouteremploi():
+    """Ajouter un emploi"""
+
+    # je verifie si l'utilisateur est connecté sinon je redirige vers la page de connexion /
+    if not session.get("user"):
+        return redirect("/", code=303)
+    else:
+        if request.method == "GET":
+            # je retourne la page du formulaire d'ajout d'emploi
+            return render_template("emploi/ajouteremploi.jinja", message={})
+        else:
+            entreprise = {"nom": "", "adresse": "", "siteWeb": ""}
+
+            # je recupere les données du formulaire
+            titre = request.form.get("titre", default="")
+            description = request.form.get("description", default="")
+            entreprise.nom = request.form.get("entrepriseNom", default="")
+            entreprise.adresse = request.form.get("entrepriseAdresse", default="")
+            entreprise.siteWeb = request.form.get("entrepriseSiteWeb", default="")
+            lieu = request.form.get("lieu", default="")
+            salaire = request.form.get("salaire", type=int, default=0)
+            user = session.get("user")
+            id_user = user["_id"]
+            print(id_user)
+            message = {}
+            # je verifie si les champs sont vides
+            if not titre or not description or not entreprise.nom or not entreprise.adresse or not entreprise.siteWeb or not lieu or not salaire:
+                message['titre'] = True,
+                message['description'] = True,
+                message['entrepriseNom'] = True,
+                message['entrepriseAdresse'] = True,
+                message['entrepriseSiteWeb'] = True,
+                message['lieu'] = True,
+                message['salaire'] = True,
+
+                return render_template("emploi/ajouteremploi.jinja", message=message)
+            else:
+                # j insere l'emploi dans la base de données
+
+                resultat = app.mongo.db.emplois.insert_one({
+                    "titre": titre,
+                    "description": description,
+                    "entreprise": entreprise,
+                    "lieu": lieu,
+                    "salaire": salaire,
+                    "userId": id_user
+                })
+
+                resultatid = resultat.inserted_id
+                object_id = ObjectId(resultatid)
+                emploi = app.mongo.db.emplois.find_one({"_id": object_id})
+                # je redirige vers la page details des emplois
+                return redirect("/emploi/détail/" + str(emploi["_id"]), code=303)
